f0829b87dd644816e7eb744c9c6b3235a894d7a2
Daniel Treviño <danieltrev09@gmail.com>
2023-11-16 16:40:40 -0600

    Banco
    Version 0

---
+++ BankConsole/BankConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("\t-------------------------------------------------------------------------------------");
+Console.WriteLine("\t---                                  <*(BankConsole)*>                            ---");
+Console.WriteLine("\t-------------------------------------------------------------------------------------");
+
+
+int M1;
+
+while (true)
+{
+    Console.WriteLine("        -----                                                                           -----\n");
+    Console.WriteLine("        ---                                1.Crear nuevo usuario                          ---\n");
+    Console.WriteLine("        -----                                                                           -----\n");
+    Console.WriteLine("        -------------------------------------------------------------------------------------");
+    Console.WriteLine("        -----                                                                           -----\n");
+    Console.WriteLine("        ---                           2.Eliminar un usuario existente                     ---\n");
+    Console.WriteLine("        -----                                                                           -----\n");
+    Console.WriteLine("        -------------------------------------------------------------------------------------");
+    Console.WriteLine("        -----                                                                           -----\n");
+    Console.WriteLine("        ---                                       3.Salir:                                ---\n");
+    Console.WriteLine("        -----                                                                           -----\n");
+    Console.WriteLine("        -------------------------------------------------------------------------------------\n");
+    M1 = int.Parse(Console.ReadLine());
+    
+
+    switch (M1)
+    {
+        case 1:
+           
+        
+
+            break;
+
+        case 2:
+           
+            break;
+
+        case 3:
+        
+            return 0;
+
+    }
+    Console.Clear();
+}
